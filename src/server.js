affb9de4b814e385846f25412e44fce27d642c09
itspoma <itspoma@gmail.com>
2018-05-02 19:52:38 +0300

    added https support

--- src/server.js
+++ src/server.js
@@ -1,15 +1,16 @@
-var http = require('http');
+var https = require('https');
 var fs = require('fs');
 var path = require('path');
 
-http.createServer(function (request, response) {
-  console.log('request ', request.url);
+var options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/ssl-testing.ralabs.org/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/ssl-testing.ralabs.org/cert.pem'),
+  ca: fs.readFileSync('/etc/letsencrypt/live/ssl-testing.ralabs.org/chain.pem')
+};
 
-  fs.readFile('./src/index.html', function(error, content) {
-    console.log('content');
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.end(content, 'utf-8');
-  });
-}).listen(8125);
+https.createServer(options, function (req, res) {
+  res.writeHead(200);
+  res.end("hello world\n");
+}).listen(8000);
 
-console.log('Server running at http://127.0.0.1:8125/');
+console.log('Server running at https://127.0.0.1:8000/');
