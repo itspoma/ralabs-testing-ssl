16252080247826b706ba4c89e6b8de21e5ae579f
itspoma <itspoma@gmail.com>
2018-05-02 19:55:55 +0300

    updated https support

--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ var fs = require('fs');
 var path = require('path');
 
 var options = {
-  key: fs.readFileSync('/etc/letsencrypt/live/ssl-testing.ralabs.org/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/ssl-testing.ralabs.org/cert.pem'),
-  ca: fs.readFileSync('/etc/letsencrypt/live/ssl-testing.ralabs.org/chain.pem')
+  key: fs.readFileSync('/home/ubuntu/www/ralabs-testing-ssl/certs/privkey.pem'),
+  cert: fs.readFileSync('/home/ubuntu/www/ralabs-testing-ssl/certs/cert.pem'),
+  ca: fs.readFileSync('/home/ubuntu/www/ralabs-testing-ssl/certs/chain.pem')
 };
 
 https.createServer(options, function (req, res) {
